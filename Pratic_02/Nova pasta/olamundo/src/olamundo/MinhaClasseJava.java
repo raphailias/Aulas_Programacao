629b0754401c76f748c4bb3f195ed3a5f0e8c1b7
RAPHAEL DE JESUS JOAO ILIAS <16.00378-0@maua.br>
2019-03-18 13:03:40 -0300

    meu deus

---
+++ Pratic_02/Nova pasta/olamundo/src/olamundo/MinhaClasseJava.java
@@ -0,0 +1,14 @@
+package olamundo;
+
+public class MinhaClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 24;
+		String nome = "everson";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+				
+	}
+
+}
